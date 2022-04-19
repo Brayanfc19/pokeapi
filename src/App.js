bd3175fa5c2a28eb1a1b83eded09388ab866e050
Brayan Farro <farrocachaybrayan@gmail.com>
2022-04-18 21:41:54 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -3,22 +3,19 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from "./Components/Layout";
 import CardPokemon from "./Components/CardPokemon"
 import Home from "./routes/Home";
-import UserProvider from "./context/UserProvider.jsx"
-
 
 function App() {
   
   return (
       <Layout>
-        <UserProvider>
           <BrowserRouter>
             <Routes >
-              <Route path="/" element={<Home />} />
-              <Route path="/:nombre" element={<CardPokemon />} />
+              <Route path="/pokeapi" element={<Home />} />
+              <Route path="/pokeapi/:nombre" element={<CardPokemon />} />
+              <Route path="/pokeapi/*" element={<h2>404</h2>} />
               <Route path="/*" element={<h2>404</h2>} />
             </Routes>
           </BrowserRouter>
-        </UserProvider>
       </Layout>
   );
 }
